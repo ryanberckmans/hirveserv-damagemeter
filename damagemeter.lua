dd72d05735944350ed7f535109ecb69cfb242ed7
Ryan Berckmans <ryanberckmans@gmail.com>
2014-11-09 03:50:50 -0500

    damagemeter.lua: sort output by % damage, use account and not display name

--- damagemeter.lua
+++ damagemeter.lua
@@ -4,11 +4,48 @@ local function cmdDamageMeterReport(client, args)
   local damageJustDone = tonumber( args )
   -- client:msg("%s%d", "Your damage meter script is working (probably :). You did this much damage (only precise when your target is level 31): ", damageJustDone)
 
-  if globalDamageMeter[client.name] == nil then
-    globalDamageMeter[client.name] = 0
+  local clientName
+
+  if client.user then
+    clientName = client.user.name
+  else
+    clientName = client.name
   end
 
-  globalDamageMeter[client.name] = globalDamageMeter[client.name] + damageJustDone
+  if globalDamageMeter[clientName] == nil then
+    globalDamageMeter[clientName] = 0
+  end
+
+  globalDamageMeter[clientName] = globalDamageMeter[clientName] + damageJustDone
+end
+
+-- https://stackoverflow.com/questions/2421695/first-character-uppercase-lua
+function firstToUpper(str)
+    return (str:gsub("^%l", string.upper))
+end
+
+-- https://stackoverflow.com/questions/15706270/sort-a-table-in-lua
+function spairs(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
 end
 
 local function renderDamageMeter()
@@ -20,9 +57,9 @@ local function renderDamageMeter()
     groupTotalDamage = groupTotalDamage + clientTotalDamage
   end
   
-  for clientName, clientTotalDamage in pairs(globalDamageMeter) do
+  for clientName, clientTotalDamage in spairs(globalDamageMeter, function(t,a,b) return t[b] < t[a] end) do
     local clientPercentDamage = clientTotalDamage * 100.0 / groupTotalDamage
-    table.insert(msg, string.format("#ly%12s #lg-     #w%4d%%", clientName, clientPercentDamage))    
+    table.insert(msg, string.format("#ly%12s #lg-     #w%4d%%", firstToUpper(clientName), clientPercentDamage))    
   end
 
   if #msg > 0 then
